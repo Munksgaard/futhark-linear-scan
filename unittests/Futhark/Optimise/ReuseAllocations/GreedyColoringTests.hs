0f10eb33e5a3e52ffc2b467c96e707c1aa406c8f
Philip Munksgaard <philip@munksgaard.me>
2020-08-27 10:27:31 +0200

    Update tests and add new

--- unittests/Futhark/Optimise/ReuseAllocations/GreedyColoringTests.hs
+++ unittests/Futhark/Optimise/ReuseAllocations/GreedyColoringTests.hs
@@ -3,6 +3,8 @@ module Futhark.Optimise.ReuseAllocations.GreedyColoringTests
   )
 where
 
+import Control.Arrow ((***))
+import Data.Function ((&))
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import qualified Futhark.Optimise.ReuseAllocations.GreedyColoring as GreedyColoring
@@ -13,28 +15,56 @@ tests :: TestTree
 tests =
   testGroup
     "GreedyColoringTests"
-    [psumTest, allIntersect, emptyGraph]
+    [psumTest, allIntersect, emptyGraph, noIntersections, differentSpaces]
 
 psumTest :: TestTree
 psumTest =
-  testCase "psumTest" $ do
+  testCase "psumTest" $
     assertEqual
       "Color simple 1-2-3 using two colors"
-      [(1 :: Int, 0), (2, 1), (3, 0)]
-      $ Map.toList $ GreedyColoring.colorGraph $ Set.fromList [(1, 2), (2, 3)]
+      ([(0, "local"), (1, "local")], [(1 :: Int, 0), (2, 1), (3, 0)])
+      $ (Map.toList *** Map.toList) $
+        GreedyColoring.colorGraph
+          (Map.fromList [(1, "local"), (2, "local"), (3, "local")])
+          $ Set.fromList [(1, 2), (2, 3)]
 
 allIntersect :: TestTree
 allIntersect =
-  testCase "allIntersect" $ do
+  testCase "allIntersect" $
     assertEqual
       "Color a graph where all values intersect"
-      [(1 :: Int, 2), (2, 1), (3, 0)]
-      $ Map.toList $ GreedyColoring.colorGraph $ Set.fromList [(1, 2), (2, 3), (1, 3)]
+      ([(0, "local"), (1, "local"), (2, "local")], [(1 :: Int, 2), (2, 1), (3, 0)])
+      $ (Map.toList *** Map.toList) $
+        GreedyColoring.colorGraph
+          (Map.fromList [(1, "local"), (2, "local"), (3, "local")])
+          $ Set.fromList [(1, 2), (2, 3), (1, 3)]
 
 emptyGraph :: TestTree
 emptyGraph =
-  testCase "emptyGraph" $ do
+  testCase "emptyGraph" $
     assertEqual
       "Color an empty graph"
-      ([] :: [(Int, Int)])
-      $ Map.toList $ GreedyColoring.colorGraph $ Set.fromList []
+      ([] :: [(Int, Char)], [] :: [(Int, Int)])
+      $ (Map.toList *** Map.toList) $ GreedyColoring.colorGraph (Map.fromList []) $ Set.fromList []
+
+noIntersections :: TestTree
+noIntersections =
+  GreedyColoring.colorGraph
+    (Map.fromList [(1, "local"), (2, "local"), (3, "local")])
+    (Set.fromList [])
+    & Map.toList *** Map.toList
+    & assertEqual
+      "Color nodes with no intersections"
+      ([(0, "local")], [(1, 0), (2, 0), (3, 0)] :: [(Int, Int)])
+    & testCase "noIntersections"
+
+differentSpaces :: TestTree
+differentSpaces =
+  GreedyColoring.colorGraph
+    (Map.fromList [(1, "a"), (2, "b"), (3, "c")])
+    (Set.fromList [])
+    & Map.toList *** Map.toList
+    & assertEqual
+      "Color nodes with no intersections but in different spaces"
+      ([(0, "c"), (1, "b"), (2, "a")], [(1, 2), (2, 1), (3, 0)] :: [(Int, Int)])
+    & testCase "differentSpaces"
