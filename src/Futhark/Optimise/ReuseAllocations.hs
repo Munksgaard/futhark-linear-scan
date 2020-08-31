45d262044f1d59e850d6e130897e4a1e9994f2ed
Philip Munksgaard <philip@munksgaard.me>
2020-08-31 09:52:31 +0200

    Formatting and cleanup

--- src/Futhark/Optimise/ReuseAllocations.hs
+++ src/Futhark/Optimise/ReuseAllocations.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Futhark.Optimise.ReuseAllocations (optimise) where
@@ -26,8 +25,6 @@ import Futhark.Pass (Pass (..), PassM)
 
 type Allocs = Map VName (SubExp, Space)
 
--- type ReuseAllocsM = ReaderT (Scope KernelsMem) (Reader VNameSource)
-
 type ReuseAllocsM = Binder KernelsMem
 
 getAllocsStm :: Stm KernelsMem -> Allocs
@@ -36,7 +33,8 @@ getAllocsStm (Let (Pattern [] [PatElem name _]) _ (Op (Alloc se sp))) =
 getAllocsStm (Let _ _ (Op (Alloc _ _))) = error "impossible"
 getAllocsStm (Let _ _ (Op (Inner (SegOp segop)))) = getAllocsSegOp segop
 getAllocsStm (Let _ _ (If _ then_body else_body _)) =
-  foldMap getAllocsStm (bodyStms then_body) <> foldMap getAllocsStm (bodyStms else_body)
+  foldMap getAllocsStm (bodyStms then_body)
+    <> foldMap getAllocsStm (bodyStms else_body)
 getAllocsStm (Let _ _ (DoLoop _ _ _ body)) =
   foldMap getAllocsStm (bodyStms body)
 getAllocsStm _ = mempty
@@ -63,8 +61,8 @@ setAllocsStm m stm@(Let _ _ (If cse then_body else_body dec)) =
     { stmExp =
         If
           cse
-          (then_body {bodyStms = fmap (setAllocsStm m) $ bodyStms then_body})
-          (else_body {bodyStms = fmap (setAllocsStm m) $ bodyStms else_body})
+          (then_body {bodyStms = setAllocsStm m <$> bodyStms then_body})
+          (else_body {bodyStms = setAllocsStm m <$> bodyStms else_body})
           dec
     }
 setAllocsStm m stm@(Let _ _ (DoLoop ctx vals form body)) =
@@ -74,22 +72,26 @@ setAllocsStm m stm@(Let _ _ (DoLoop ctx vals form body)) =
           ctx
           vals
           form
-          (body {bodyStms = fmap (setAllocsStm m) $ bodyStms body})
+          (body {bodyStms = setAllocsStm m <$> bodyStms body})
     }
 setAllocsStm _ stm = stm
 
-setAllocsSegOp :: Map VName SubExp -> SegOp lvl KernelsMem -> SegOp lvl KernelsMem
+setAllocsSegOp ::
+  Map VName SubExp ->
+  SegOp lvl KernelsMem ->
+  SegOp lvl KernelsMem
 setAllocsSegOp m (SegMap lvl sp tps body) =
-  SegMap lvl sp tps $ body {kernelBodyStms = fmap (setAllocsStm m) $ kernelBodyStms body}
+  SegMap lvl sp tps $
+    body {kernelBodyStms = setAllocsStm m <$> kernelBodyStms body}
 setAllocsSegOp m (SegRed lvl sp segbinops tps body) =
   SegRed lvl sp segbinops tps $
-    body {kernelBodyStms = fmap (setAllocsStm m) $ kernelBodyStms body}
+    body {kernelBodyStms = setAllocsStm m <$> kernelBodyStms body}
 setAllocsSegOp m (SegScan lvl sp segbinops tps body) =
   SegScan lvl sp segbinops tps $
-    body {kernelBodyStms = fmap (setAllocsStm m) $ kernelBodyStms body}
+    body {kernelBodyStms = setAllocsStm m <$> kernelBodyStms body}
 setAllocsSegOp m (SegHist lvl sp segbinops tps body) =
   SegHist lvl sp segbinops tps $
-    body {kernelBodyStms = fmap (setAllocsStm m) $ kernelBodyStms body}
+    body {kernelBodyStms = setAllocsStm m <$> kernelBodyStms body}
 
 invertMap :: (Ord v, Ord k) => Map k v -> Map v (Set k)
 invertMap m =
@@ -107,32 +109,56 @@ maxSubExp = helper . Set.toList
       return s
     helper [] = error "impossible"
 
-optimiseKernel :: Interference.Graph VName -> SegOp lvl KernelsMem -> ReuseAllocsM (SegOp lvl KernelsMem)
+optimiseKernel ::
+  Interference.Graph VName ->
+  SegOp lvl KernelsMem ->
+  ReuseAllocsM (SegOp lvl KernelsMem)
 optimiseKernel graph segop = do
   let allocs = getAllocsSegOp segop
-      (colorspaces, coloring) = GreedyColoring.colorGraph (fmap snd allocs) graph
-  (maxes, maxstms) <- collectStms $ mapM (maxSubExp . Set.map (fst . (allocs !))) $ Map.elems $ invertMap coloring
-  (colors, stms) <- collectStms $ mapM (\(i, x) -> letSubExp "color" $ Op $ Alloc x $ colorspaces ! i) $ zip [0 ..] $ assert (length maxes == Map.size colorspaces) maxes
-  let segop' = setAllocsSegOp ((fmap (colors !!) coloring)) segop
+      (colorspaces, coloring) =
+        GreedyColoring.colorGraph
+          (fmap snd allocs)
+          graph
+  (maxes, maxstms) <-
+    invertMap coloring
+      & Map.elems
+      & mapM (maxSubExp . Set.map (fst . (allocs !)))
+      & collectStms
+  (colors, stms) <-
+    assert (length maxes == Map.size colorspaces) maxes
+      & zip [0 ..]
+      & mapM (\(i, x) -> letSubExp "color" $ Op $ Alloc x $ colorspaces ! i)
+      & collectStms
+  let segop' = setAllocsSegOp (fmap (colors !!) coloring) segop
   return $ case segop' of
-    SegMap lvl sp tps body -> SegMap lvl sp tps $ body {kernelBodyStms = maxstms <> stms <> kernelBodyStms body}
-    SegRed lvl sp binops tps body -> SegRed lvl sp binops tps $ body {kernelBodyStms = maxstms <> stms <> kernelBodyStms body}
-    SegScan lvl sp binops tps body -> SegScan lvl sp binops tps $ body {kernelBodyStms = maxstms <> stms <> kernelBodyStms body}
-    SegHist lvl sp binops tps body -> SegHist lvl sp binops tps $ body {kernelBodyStms = maxstms <> stms <> kernelBodyStms body}
+    SegMap lvl sp tps body ->
+      SegMap lvl sp tps $
+        body {kernelBodyStms = maxstms <> stms <> kernelBodyStms body}
+    SegRed lvl sp binops tps body ->
+      SegRed lvl sp binops tps $
+        body {kernelBodyStms = maxstms <> stms <> kernelBodyStms body}
+    SegScan lvl sp binops tps body ->
+      SegScan lvl sp binops tps $
+        body {kernelBodyStms = maxstms <> stms <> kernelBodyStms body}
+    SegHist lvl sp binops tps body ->
+      SegHist lvl sp binops tps $
+        body {kernelBodyStms = maxstms <> stms <> kernelBodyStms body}
 
-onKernels :: (SegOp SegLevel KernelsMem -> ReuseAllocsM (SegOp SegLevel KernelsMem)) -> Stms KernelsMem -> ReuseAllocsM (Stms KernelsMem)
-onKernels f stms =
-  mapM helper stms
+onKernels ::
+  (SegOp SegLevel KernelsMem -> ReuseAllocsM (SegOp SegLevel KernelsMem)) ->
+  Stms KernelsMem ->
+  ReuseAllocsM (Stms KernelsMem)
+onKernels f =
+  mapM helper
   where
-    -- helper :: (LocalScope KernelsMem m, MonadFreshNames m) => Stm KernelsMem -> m (Stm KernelsMem)
     helper stm@Let {stmExp = Op (Inner (SegOp segop))} =
       inScopeOf stm $ do
         exp' <- f segop
         return $ stm {stmExp = Op $ Inner $ SegOp exp'}
     helper stm@Let {stmExp = If c then_body else_body dec} =
       inScopeOf stm $ do
-        then_body_stms <- f `onKernels` (bodyStms then_body)
-        else_body_stms <- f `onKernels` (bodyStms else_body)
+        then_body_stms <- f `onKernels` bodyStms then_body
+        else_body_stms <- f `onKernels` bodyStms else_body
         return $
           stm
             { stmExp =
@@ -144,8 +170,8 @@ onKernels f stms =
             }
     helper stm@Let {stmExp = DoLoop ctx vals form body} =
       inScopeOf stm $ do
-        bodyStms <- f `onKernels` bodyStms body
-        return $ stm {stmExp = DoLoop ctx vals form (body {bodyStms = bodyStms})}
+        stms <- f `onKernels` bodyStms body
+        return $ stm {stmExp = DoLoop ctx vals form (body {bodyStms = stms})}
     helper stm =
       inScopeOf stm $ return stm
 
@@ -154,12 +180,22 @@ optimise =
   Pass "reuse allocations" "reuse allocations" $ \prog ->
     let (lumap, _) = LastUse.analyseProg prog
         (_, _, graph) =
-          foldMap (\f -> runReader (Interference.analyseKernels lumap (bodyStms $ funDefBody f)) $ scopeOf f) $
-            progFuns prog
+          foldMap
+            ( \f ->
+                runReader
+                  ( Interference.analyseKernels lumap $
+                      bodyStms $ funDefBody f
+                  )
+                  $ scopeOf f
+            )
+            $ progFuns prog
      in Pass.intraproceduralTransformation (onStms graph) prog
   where
-    onStms :: Interference.Graph VName -> Scope KernelsMem -> Stms KernelsMem -> PassM (Stms KernelsMem)
+    onStms ::
+      Interference.Graph VName ->
+      Scope KernelsMem ->
+      Stms KernelsMem ->
+      PassM (Stms KernelsMem)
     onStms graph scope stms = do
       let m = localScope scope $ optimiseKernel graph `onKernels` stms
-      res <- fmap fst $ modifyNameSource $ runState (runBinderT m mempty)
-      return res
+      fmap fst $ modifyNameSource $ runState (runBinderT m mempty)
